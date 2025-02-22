aa9fde2e8737e4ea7b4bea82948e693adaeff79d
glorycode02 <glorycode02@gmail.com>
2025-02-22 11:47:54 +0200

    story genie

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,91 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import React from 'react'
+import { FaGithub, FaGoogle } from 'react-icons/fa'
+
+const Signup = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+                <h2 className="text-2xl font-bold text-center">Story <span className='text-green-600'>Genie</span></h2>
+                <p className='text-gray-400 text-center'>Come join a large community of story tellers</p>
+                <form className="space-y-4">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                            Email address
+                        </label>
+                        <Input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                            Password
+                        </label>
+                        <Input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">
+                            Confirm Password
+                        </label>
+                        <Input
+                            id="confirm-password"
+                            name="confirm-password"
+                            type="password"
+                            required
+                            className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                        />
+                    </div>
+                    <div>
+                        <Button
+                            type="submit"
+                            className="w-full px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Sign Up
+                        </Button>
+                    </div>
+                    <div className="text-center">
+                        <p className="text-sm text-gray-600">
+                            Already have an account?{' '}
+                            <a href="/login" className="font-medium text-green-600 hover:text-green-500">x
+                                Login
+                            </a>
+                        </p>
+                    </div>
+                    <div className="relative my-6">
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300"></div>
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="px-2 bg-white text-gray-500">Or continue with</span>
+                        </div>
+                    </div>
+                    <div className="flex justify-center space-x-4">
+                        <Button className="w-full px-4 py-2 text-sm font-medium text-white bg-gray-800  border border-transparent rounded-md shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                            <FaGoogle />
+                            Google
+                        </Button>
+                        <Button className="w-full px-4 py-2 text-sm font-medium text-white bg-gray-800 border border-transparent rounded-md shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                            <FaGithub />
+                            GitHub
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
